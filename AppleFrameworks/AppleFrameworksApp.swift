1bc245f54bbdb5f4e5b7d513b8fb932e2144309c
Daniel Karath <karath.dani@gmail.com>
2023-02-01 11:10:00 +0100

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Daniel Karath on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
